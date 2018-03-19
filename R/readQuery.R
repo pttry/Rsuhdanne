02952c487c633a0e89176317fe09ff8130e5d444
Janne Huovari <janne.huovari@ptt.fi>
2018-03-19 16:34:17 +0200

    Delete old

--- R/readQuery.R
+++
@@ -1,7 +0,0 @@
-#' @rdname writeQuery
-#' @export
-readQuery <- function(group, datab){
-  qfile <- file.path(hakut_dir(), datab, paste(group, "r", sep = "."))
-  lst <- dget(qfile)
-  return(lst)
-}
